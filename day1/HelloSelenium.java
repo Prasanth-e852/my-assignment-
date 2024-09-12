3d7c6655f03a892f26b45e475ee3bfee9dcfb26c
Prasanth-e852 <prasanth8985659058@gmail.com>
2024-09-12 10:20:39 +0530

    Add files via upload
    Day1

---
+++ day1/HelloSelenium.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class HelloSelenium {
+	
+
+	public static void main(String[] args) {
+		int i =10;
+		System.out.println(i);
+		HelloSelenium b = new HelloSelenium();
+System.out.println( b.add(10,20));
+	}
+public int add(int a, int b) {
+	int c =a +b ;
+	return c;
+}
+}
